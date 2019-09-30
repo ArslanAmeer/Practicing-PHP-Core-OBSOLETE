091cf73a55dc8872fc665e84de061025012791a8
ArslanAmeer <arslan.ameer@hotmail.com>
2019-09-30 17:55:16 +0500

    ✨✔👍 Update: User Registration with with Ajax Successfully

--- index.php
+++ index.php
@@ -14,82 +14,96 @@
         <title>PHP Practice</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-        </script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-        </script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-        </script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     </head>
 
     <body>
-
-        <!-- PHP CODE HERE -->
-        <div>
-            <?php
-                if (isset($_POST['register'])) {
-                    $firstName = $_POST["firstName"];
-                    $lastName = $_POST["lastName"];
-                    $email = $_POST["email"];
-                    $password = $_POST["password"];
-                    $phone = $_POST["phone"];
-
-                    $sql = "INSERT INTO users(firstName, lastName, email, password, phone) VALUES (?,?,?,?,?)";
-                    $sqlstmnt = $db->prepare($sql);
-                    $result = $sqlstmnt->execute([$firstName, $lastName, $email, $password, $phone]);
-                    if ($result) {
-                        echo "Data Added Successfully!";
-                    } else 
-                    
-                    {
-                        echo "Error While Saving Data!";
-                    }
-            
-                }
-            ?>
-        </div>
-
         <!-- A Simple Basic Registration Form -->
         <!-- Registration Form -->
-        <form action="index.php" method="POST">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-6">
-                        <h1 class="text-center mt-2">REGISTER HERE</h1>
-                        <hr>
-                        <div class="form-group">
-                            <label for="firstName">First Name:</label>
-                            <input type="text" class="form-control" id="firstName" name="firstName" required>
+        <div>
+            <form id="regForm" action="register.php" method="post">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-6">
+                            <h1 class="text-center mt-2">REGISTER HERE</h1>
+                            <hr>
+                            <div class="form-group">
+                                <label for="firstName">First Name:</label>
+                                <input type="text" class="form-control" id="firstName" name="firstName" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="lastName">Last Name:</label>
+                                <input type="text" class="form-control" id="lastName" name="lastName" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email address:</label>
+                                <input type="email" class="form-control" id="email" name="email" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password:</label>
+                                <input type="password" class="form-control" id="password" name="password" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="phone">Phone:</label>
+                                <input type="text" class="form-control" id="phone" name="phone" required>
+                            </div>
+                            <input type="submit" name="register" id="register_btn" class="btn btn-primary"
+                                value="Submit" />
                         </div>
-                        <div class="form-group">
-                            <label for="lastName">Last Name:</label>
-                            <input type="text" class="form-control" id="lastName" name="lastName" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Email address:</label>
-                            <input type="email" class="form-control" id="email" name="email" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Password:</label>
-                            <input type="password" class="form-control" id="password" name="password" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="phone">Phone:</label>
-                            <input type="text" class="form-control" id="phone" name="phone" required>
-                        </div>
-                        <button type="submit" name="register" class="btn btn-primary">Submit</button>
                     </div>
                 </div>
-            </div>
-        </form>
+            </form>
+        </div>
 
-        <!-- JavaScript -->
-        <script>
-        $(document).ready(() => {
-            alert("HELLO")
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+        <!-- <script src="https://cdn.jsdelivr.net/npm/promise-polyfill"></script> -->
+        <script type="text/javascript">
+        $(() => {
+            $("#register_btn").click(function(e) {
+                var valid = this.form.checkValidity();
+                debugger;
+                if (valid) {
+                    var firstname = $("#firstName").val();
+                    var lastname = $("#lastName").val();
+                    var email = $("#email").val();
+                    var password = $("#password").val();
+                    var phone = $("#phone").val();
+
+                    e.preventDefault(e);
+
+                    $.ajax({
+                        type: 'POST',
+                        url: 'register.php',
+                        data: {
+                            firstName: firstname,
+                            lastName: lastname,
+                            email: email,
+                            password: password,
+                            phone: phone
+                        },
+                        success: function(data) {
+                            Swal.fire({
+                                'title': 'Successful',
+                                'text': data,
+                                'type': 'success',
+                                onClose: () => {
+                                    $('#regForm').trigger("reset");
+                                }
+                            });
+
+                        },
+                        error: function(e) {
+                            Swal.fire({
+                                'title': 'Error',
+                                'text': 'There were errors while saving the data',
+                                'type': 'error'
+                            })
+                        }
+                    });
+
+                } else {}
+
+            });
         });
         </script>
     </body>
