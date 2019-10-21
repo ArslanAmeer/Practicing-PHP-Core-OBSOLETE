27e8e42d829352655d41cdf60682bfc824840f77
ArslanAmeer <arslan.ameer@hotmail.com>
2019-10-21 16:20:40 +0500

    🛠♻👍 Update: navBar Link Update

--- views/sub-views/navbar.php
+++ views/sub-views/navbar.php
@@ -9,20 +9,20 @@
             <ul class="navbar-custom navbar-nav ml-auto">
 
                 <li class="nav-item active">
-                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="../index.php">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <!-- PHP Script - Link based on User Login -->
                 <?php
                     if (!isset($_SESSION['userLogin'])) {
                         echo ' <li class="nav-item">
-                                    <a class="nav-link" href="login.php">Login</a>
+                                    <a class="nav-link" href="../login.php">Login</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="register.php">Register</a>
+                                    <a class="nav-link" href="../register.php">Register</a>
                                 </li>';
                     } else {
                         echo '<li class="nav-item">
-                                    <a class="nav-link" href="process/logout_Process.php">Logout</a>
+                                    <a class="nav-link" href="~/process/logout_Process.php">Logout</a>
                                 </li>';
                     }
                     
