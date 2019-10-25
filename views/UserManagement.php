26eed9542794f8a5fb166ffb32c20b1a0caaad4f
ArslanAmeer <arslan.ameer@hotmail.com>
2019-10-25 16:35:55 +0500

    ♻👍 Update: Title Change to User & Remove 'Add button'

--- views/UserManagement.php
+++ views/UserManagement.php
@@ -10,7 +10,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Content Management :: Arslan Demo</title>
+        <title>User Management :: Arslan Demo</title>
         <!-- <link
             href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i|Open+Sans:300,300i,400,400i,700,700i"
             rel="stylesheet"> -->
@@ -33,7 +33,6 @@
                         <h4 class="text-center">
                             <mark> Basic CRUD Operations in CorePHP </mark>
                         </h4>
-                        <button id="addBtn" class="btn btn-primary"> Add</button>
                     </div>
                     <hr>
                     <!-- Datatable here -->
